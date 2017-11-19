46666afbc983279eb69ad6489ad9ef0f6d673750
Sabrina Jones <sabrina.jones87@gmail.com>
2017-11-19 06:41:02 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
-  accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
+  accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes['name'].blank?}
 end
